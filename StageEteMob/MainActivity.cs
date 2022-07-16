74f86478d39e8b039555041c2f69660594512297
T0UCHM3 <adolf.hazmi@yahoo.com>
2022-07-16 23:06:49 +0100

    Splitting view, one for adding and one for retrieving

--- StageEteMob/MainActivity.cs
+++ StageEteMob/MainActivity.cs
@@ -13,25 +13,35 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json.Linq;
 using System.Text;
+using Google.Android.Material.BottomNavigation;
+using Google.Android.Material.Tabs;
+using Android.Widget;
+
 
 namespace StageEteMob
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
+    //BottomNavigationView.IOnNavigationItemSelectedListener
     {
+        //ViewStub stub;
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            AndroidX.AppCompat.Widget.Toolbar toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
+            //set setfrag as main frag
+            SupportFragmentManager.BeginTransaction().Add(Resource.Id.containerView, new SetFrag()).Commit();
 
+            TabLayout tab = FindViewById<TabLayout>(Resource.Id.tabLayout1);
+            tab.TabSelected += TabLayoutOnTabSelected;
 
-            MaterialButton btn = FindViewById<MaterialButton>(Resource.Id.button1);
-            btn.Click += testClick;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -40,6 +50,20 @@ namespace StageEteMob
             return true;
         }
 
+        private void TabLayoutOnTabSelected(object sender, TabLayout.TabSelectedEventArgs tabSelectedEventArgs)
+        {
+            string selected = tabSelectedEventArgs.Tab.Text;
+            if (selected == "set")
+            {
+                Console.WriteLine("*******************   set ");
+                SupportFragmentManager.BeginTransaction().Replace(Resource.Id.containerView, new SetFrag()).Commit();
+            }
+            else
+            {
+                Console.WriteLine("*******************   get ");
+                SupportFragmentManager.BeginTransaction().Replace(Resource.Id.containerView, new GetFrag()).Commit();
+            }
+        }
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
             int id = item.ItemId;
@@ -52,103 +76,6 @@ namespace StageEteMob
         }
 
 
-        void testClick(object sender, EventArgs eventArgs)
-        {
-            AppCompatEditText nomET = FindViewById<AppCompatEditText>(Resource.Id.nomTF);
-            AppCompatEditText telET = FindViewById<AppCompatEditText>(Resource.Id.telTF);
-            AppCompatEditText paysET = FindViewById<AppCompatEditText>(Resource.Id.paysTF);
-
-
-            Console.WriteLine("*******************   " + nomET.Text);
-            var data =new
-            {
-                Tel = telET.Text,
-                Nom = nomET.Text,
-                Pays = paysET.Text
-            };
-            var json = JsonConvert.SerializeObject(data);
-            meth(json);
-        }
-        async void meth(string val)
-        {
-            //await CallAPI();
-            
-            await SetAPI(val);
-        }
-
-        private async Task CallAPI()
-        {
-            try
-            {
-                string uri = "";
-
-                //only for testing with current emulator 
-                if (Build.Hardware.Contains("ranchu"))
-                {
-                    uri = "https://10.0.2.2:44317/api/Values";
-                    Console.WriteLine("********* ranchu *******");
-                }
-                else
-                    uri = "https://192.168.1.2:45456/api/Values";
-
-                //bypassing SSLHandshakeException
-                HttpClientHandler clientHandler = new HttpClientHandler
-                {
-                    ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
-                };
-
-                HttpClient httpClient = new HttpClient(clientHandler);
-
-                HttpResponseMessage httpResponse = await httpClient.GetAsync(uri);
-
-                if (httpResponse.IsSuccessStatusCode)
-                {
-                    string result = await httpResponse.Content.ReadAsStringAsync();
-                    var JsonString = JsonConvert.DeserializeObject<string>(result);
-                    string prettyJson = JToken.Parse(JsonString).ToString(Formatting.Indented);
-
-                    Console.WriteLine("************** Result: " + prettyJson);
-
-                }
-                else
-                {
-                    Console.WriteLine("************** failed " + httpResponse.StatusCode + " " + httpResponse.ReasonPhrase);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("************** Error Message: " + ex.Message);
-                Console.WriteLine("************** Stack Trace: " + ex.StackTrace);
-            }
-        }
-        private async Task SetAPI(string valueToSend)
-        {
-            string uri = "";
-            //    //only for testing with current emulator 
-            if (Build.Hardware.Contains("ranchu"))
-                uri = "https://10.0.2.2:44317/api/Values";
-            else
-                uri = "https://192.168.1.2:45456/api/Values";
-
-
-            //bypassing SSLHandshakeException
-            HttpClientHandler clientHandler = new HttpClientHandler
-            {
-                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
-            };
-
-            var client = new HttpClient(clientHandler);
-            var httpContent = new StringContent("");
-            var result = await client.PostAsync(uri + "?fromMob="+valueToSend, httpContent);
-
-
-            if (result.IsSuccessStatusCode)
-            {
-                var tokenJson = await result.Content.ReadAsStringAsync();
-            }
-            else
-                Console.WriteLine("************** FAILED: " + result.ReasonPhrase);
-        }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
---
+++ StageEteMob/SetFrag.cs
@@ -0,0 +1,137 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Google.Android.Material.Button;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AndroidX.AppCompat.App;
+using AndroidX.AppCompat.Widget;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace StageEteMob
+{
+    public class SetFrag : AndroidX.Fragment.App.Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+
+            var fragmentView = inflater.Inflate(Resource.Layout.content_main, container, false);
+
+            MaterialButton btn = fragmentView.FindViewById<MaterialButton>(Resource.Id.button1);
+            btn.Click += initEvent;
+
+            return fragmentView;
+        }
+        private void initEvent(object sender, EventArgs eventArgs)
+        {
+            AppCompatEditText nomET = View.FindViewById<AppCompatEditText>(Resource.Id.nomTF);
+            AppCompatEditText telET = View.FindViewById<AppCompatEditText>(Resource.Id.telTF);
+            AppCompatEditText paysET = View.FindViewById<AppCompatEditText>(Resource.Id.paysTF);
+
+            Console.WriteLine("*******************   " + nomET.Text);
+            var data = new
+            {
+                Tel = telET.Text,
+                Nom = nomET.Text,
+                Pays = paysET.Text
+            };
+            var json = JsonConvert.SerializeObject(data);
+            selector(json);
+        }
+
+        private async void selector(string val)
+        {
+            //await CallAPI();
+
+            await SetAPI(val);
+        }
+
+        private async Task CallAPI()
+        {
+            try
+            {
+                string uri = "";
+
+                //only for testing with current emulator
+                if (Build.Hardware.Contains("ranchu"))
+                {
+                    uri = "https://10.0.2.2:44317/api/Values";
+                    Console.WriteLine("********* ranchu *******");
+                }
+                else
+                    uri = "https://192.168.1.2:45456/api/Values";
+
+                //bypassing SSLHandshakeException
+                HttpClientHandler clientHandler = new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
+                };
+
+                HttpClient httpClient = new HttpClient(clientHandler);
+
+                HttpResponseMessage httpResponse = await httpClient.GetAsync(uri);
+
+                if (httpResponse.IsSuccessStatusCode)
+                {
+                    string result = await httpResponse.Content.ReadAsStringAsync();
+                    var JsonString = JsonConvert.DeserializeObject<string>(result);
+                    string prettyJson = JToken.Parse(JsonString).ToString(Formatting.Indented);
+
+                    Console.WriteLine("************** Result: " + prettyJson);
+                }
+                else
+                {
+                    Console.WriteLine("************** failed " + httpResponse.StatusCode + " " + httpResponse.ReasonPhrase);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("************** Error Message: " + ex.Message);
+                Console.WriteLine("************** Stack Trace: " + ex.StackTrace);
+            }
+        }
+
+        private async Task SetAPI(string valueToSend)
+        {
+            string uri = "";
+            //only for testing with current emulator
+            if (Build.Hardware.Contains("ranchu"))
+                uri = "https://10.0.2.2:44317/api/Values";
+            else
+                uri = "https://192.168.1.2:45456/api/Values";
+
+            //bypassing SSLHandshakeException
+            HttpClientHandler clientHandler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
+            };
+
+            var client = new HttpClient(clientHandler);
+            var httpContent = new StringContent("");
+            var result = await client.PostAsync(uri + "?fromMob=" + valueToSend, httpContent);
+
+            if (result.IsSuccessStatusCode)
+            {
+                var tokenJson = await result.Content.ReadAsStringAsync();
+            }
+            else
+                Console.WriteLine("************** FAILED: " + result.ReasonPhrase);
+        }
+    }
+}
